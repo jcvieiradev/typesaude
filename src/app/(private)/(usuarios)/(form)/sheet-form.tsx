46b8a63c3371262e72cb5bde6ea39ccfca312b1a
Julio <jcvieiradev@gmail.com>
2024-09-09 20:49:46 -0300

    Iniciado Layout para formulários

---
+++ src/app/(private)/(usuarios)/(form)/sheet-form.tsx
@@ -0,0 +1,34 @@
+import { InputForm } from "@/components/form/input-form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+interface SheetFormProps {}
+
+export function SheetForm({}: SheetFormProps) {
+  return (
+    <SheetContent className="min-w-[500px]">
+      <SheetHeader>
+        <SheetTitle>Novo Usuário</SheetTitle>
+        <SheetDescription>Adicione um novo usuário</SheetDescription>
+      </SheetHeader>
+      <ScrollArea className="h-[calc(100vh-100px)]">
+        <form className="py-4 px-2 space-y-2">
+          <InputForm label="Nome" name="name" />
+          <InputForm label="Email" name="Email" type="email" />
+          <InputForm label="Senha" name="Senha" type="password" />
+          <div className="flex justify-end">
+            <Button className="w-32">Salvar</Button>
+          </div>
+        </form>
+      </ScrollArea>
+    </SheetContent>
+  );
+}
---
+++ src/components/form/input-form.tsx
@@ -0,0 +1,15 @@
+import { Input, InputProps } from "../ui/input";
+import { Label } from "../ui/label";
+
+interface InputFormProps {
+  label: string;
+}
+
+export function InputForm({ label, ...props }: InputFormProps & InputProps) {
+  return (
+    <div className="grid grid-cols-4 items-center gap-4">
+      <Label className="col-span-1 text-right">{label}</Label>
+      <Input className="col-span-3" placeholder={label} {...props} />
+    </div>
+  );
+}
